b533dba55b1e98f43ac5ff17c09c200e702d6324
Jose Espinoza <112590169+DrVillain@users.noreply.github.com>
2026-03-20 13:57:50 -0400

    Create most_popular_products.sql
    Analysis script that breaks down revenue, units sold and transactions by product and category ordered by revenue within each category

---
+++ analysis/most_popular_products.sql
@@ -0,0 +1,19 @@
+-- Popular Products: breaks down revenue, units sold and transactions by product and category
+-- ordered by product category alphabetically and then by revenue within each category
+-- NULL categories are pushed to the bottom
+-- intended to identify best selling products and categories for business insights
+
+SELECT
+    product_category,
+    product_name,
+    CAST(SUM(quantity * price) AS DECIMAL(10,2)) AS product_revenue,
+    SUM(quantity) AS product_units_sold,
+    COUNT(transaction_id) AS product_transactions
+
+FROM dbo.cleaned_transactions
+GROUP BY product_category, product_name
+ORDER BY 
+    CASE 
+        WHEN product_category IS NULL THEN 1 ELSE 0 END,
+    product_category,
+    product_revenue DESC;
