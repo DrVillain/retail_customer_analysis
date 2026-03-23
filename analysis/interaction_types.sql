d4e9c17af7852a76d6076d82005d3398b18ddfef
Jose Espinoza <112590169+DrVillain@users.noreply.github.com>
2026-03-23 11:26:16 -0400

    Update interaction_types.sql

--- analysis/interaction_types.sql
+++ analysis/interaction_types.sql
@@ -1,6 +1,5 @@
 -- Interaction Types: counts the total interactions by type and channel
 -- NULL interaction types are labeled as 'Other' since they cannot be assumed
--- NULL channels are filtered out since they cannot be attributed to any channel
 -- ordered by interaction type and then by total interactions within each type
 
 SELECT
@@ -8,7 +7,6 @@ SELECT
     channel,
     COUNT(interaction_id) AS total_interactions
 FROM dbo.cleaned_interactions
-WHERE channel IS NOT NULL
 GROUP BY interaction_type, channel
 ORDER BY
     interaction_type, total_interactions DESC;
